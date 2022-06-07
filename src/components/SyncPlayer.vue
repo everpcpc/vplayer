c9176406496399266f4906c63de7fc6422a4eb9a
everpcpc <git@everpcpc.com>
2022-06-07 22:03:41 +0800

    fix: adjust player

--- src/components/SyncPlayer.vue
+++ src/components/SyncPlayer.vue
@@ -2,10 +2,6 @@
   <v-container>
     <v-card>
       <div id="dplayer"></div>
-      <v-card-actions>
-        <v-btn @click="onPlay">play</v-btn>
-        <v-btn @click="onPause">pause</v-btn>
-      </v-card-actions>
     </v-card>
   </v-container>
 </template>
@@ -23,13 +19,31 @@ onMounted(() => {
     video: {
       url: "https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4",
     },
+    contextmenu: [
+      {
+        text: "sync",
+        click: (player) => {
+          console.log(player.video.currentTime);
+          console.log(player.video.currentSrc);
+          player.notice("synced", 2000, 0.8);
+        },
+      },
+    ],
+  });
+  dp.on("play", function () {
+    console.log("play");
+  });
+  dp.on("pause", function () {
+    console.log("pause");
+  });
+  //   dp.on("progress", function (event) {
+  //     console.log("progress", event);
+  //   });
+  dp.on("seeked", function () {
+    console.log("seeked");
+  });
+  dp.on("ratechange", function () {
+    console.log("ratechange");
   });
 });
-
-function onPlay() {
-  dp.play();
-}
-function onPause() {
-  dp.pause();
-}
 </script>
