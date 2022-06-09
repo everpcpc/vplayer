a49e2978ce97ae3ec94b0835f120a45c145adb61
everpcpc <git@everpcpc.com>
2022-06-09 22:58:28 +0800

    fix: do not use vue setup

--- src/components/SyncPlayer.vue
+++ src/components/SyncPlayer.vue
@@ -3,151 +3,181 @@
     <v-card>
       <div id="dplayer"></div>
       <v-card-actions>
-        <v-btn class="primary" @click="browse">browse</v-btn>
+        <v-btn color="primary">
+          browse
+          <v-dialog v-model="browseDialog" max-width="600px" activator="parent">
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">User Profile</span>
+              </v-card-title>
+              <v-card-text>
+                <v-treeview :items="items" :search="search" :filter="filter">
+                  <template v-slot:prepend="{ item }">
+                    <v-icon
+                      v-if="item.children"
+                      v-text="
+                        `mdi-${
+                          item.id === 1 ? 'home-variant' : 'folder-network'
+                        }`
+                      "
+                    ></v-icon>
+                  </template>
+                </v-treeview>
+              </v-card-text>
+            </v-card>
+          </v-dialog>
+        </v-btn>
       </v-card-actions>
       <v-card-text> </v-card-text>
     </v-card>
   </v-container>
 </template>
 
-<script setup>
-import { onMounted } from "vue";
+<script>
 import DPlayer from "dplayer";
 import { io } from "socket.io-client";
 
-let ignoreEvents = {
-  seek: 0,
-  play: 0,
-  pause: 0,
-  ratechange: 0,
-};
-
-let socket = null;
-let playlist = [];
-
-function randomString(length) {
-  let str = "";
-  for (let i = 0; i < length; i++) {
-    str += Math.random().toString(36).substring(2);
-  }
-  return str.substring(0, length);
-}
-
-function browse() {
-  socket.emit("browse", (res) => {
-    console.log(res);
-  });
-}
-
-function resultHandler(player, event) {
-  if (event.src !== player.video.currentSrc) {
-    player.switchVideo({ url: event.src });
-    player.notice(`switched to ${event.src}`, 2000, 0.8);
-  }
-  switch (event.action) {
-    case "play":
-      ignoreEvents.seek++;
-      player.seek(event.time + 0.2); // +0.2s for network delay
-      ignoreEvents.play++;
-      player.play();
-      break;
-    case "pause":
-      ignoreEvents.seek++;
-      player.seek(event.time);
-      ignoreEvents.pause++;
-      player.pause();
-      break;
-    case "seek":
-      ignoreEvents.seek++;
-      player.seek(event.time);
-      break;
-    case "sync":
-      if (event.paused) {
-        ignoreEvents.pause++;
-        player.pause();
-      } else {
-        ignoreEvents.play++;
-        player.play();
-      }
-      ignoreEvents.seek++;
-      player.seek(event.time);
-      break;
-  }
-}
-
-const userID = randomString(10);
-
-function sendControl(player, socket, action) {
-  socket.emit(
-    "video-control",
-    JSON.stringify({
-      user: userID,
-      action: action,
-      time: player.video.currentTime,
-      src: player.video.currentSrc,
-      paused: player.video.paused,
-    })
-  );
-}
-
-onMounted(() => {
-  socket = io();
-
-  const dp = new DPlayer({
-    container: document.getElementById("dplayer"),
-    screenshot: true,
-    volume: 0,
-    video: {
-      type: "auto",
-    },
-    contextmenu: [
-      {
-        text: "Sync",
-        click: (player) => {
-          sendControl(player, socket, "sync");
-          player.notice("synced", 2000, 0.8);
-        },
+export default {
+  data() {
+    return {
+      userID: "",
+      socket: null,
+      dp: null,
+      playlist: [],
+      browseDialog: false,
+      ignoreEvents: {
+        seek: 0,
+        play: 0,
+        pause: 0,
+        ratechange: 0,
       },
-    ],
-  });
+      files: [],
+    };
+  },
 
-  socket.on("video-control", (res) => {
-    const result = JSON.parse(res);
-    if (result.user !== userID) {
-      resultHandler(dp, result);
-    }
-  });
+  methods: {
+    randomString(length) {
+      let str = "";
+      for (let i = 0; i < length; i++) {
+        str += Math.random().toString(36).substring(2);
+      }
+      return str.substring(0, length);
+    },
 
-  dp.on("play", function () {
-    if (ignoreEvents.play > 0) {
-      ignoreEvents.play--;
-      return;
-    }
-    sendControl(dp, socket, "play");
-  });
-  dp.on("pause", function () {
-    if (ignoreEvents.pause > 0) {
-      ignoreEvents.pause--;
-      return;
-    }
-    sendControl(dp, socket, "pause");
-  });
-  // dp.on("progress", function (event) {
-  //   console.log("progress", event);
-  // });
-  dp.on("seeked", function () {
-    if (ignoreEvents.seek > 0) {
-      ignoreEvents.seek--;
-      return;
-    }
-    sendControl(dp, socket, "seek");
-  });
-  dp.on("ratechange", function () {
-    if (ignoreEvents.ratechange > 0) {
-      ignoreEvents.ratechange--;
-      return;
-    }
-    console.log("ratechange");
-  });
-});
+    refreshFiles() {
+      this.socket.emit("browse", (res) => {
+        this.files = JSON.parse(res);
+      });
+    },
+
+    resultHandler(event) {
+      if (event.src !== this.dp.video.currentSrc) {
+        this.dp.switchVideo({ url: event.src });
+        this.dp.notice(`switched to ${event.src}`, 2000, 0.8);
+      }
+      switch (event.action) {
+        case "play":
+          this.ignoreEvents.seek++;
+          this.dp.seek(event.time + 0.2); // +0.2s for network delay
+          this.ignoreEvents.play++;
+          this.dp.play();
+          break;
+        case "pause":
+          this.ignoreEvents.seek++;
+          this.dp.seek(event.time);
+          this.ignoreEvents.pause++;
+          this.dp.pause();
+          break;
+        case "seek":
+          this.ignoreEvents.seek++;
+          this.dp.seek(event.time);
+          break;
+        case "sync":
+          if (event.paused) {
+            this.ignoreEvents.pause++;
+            this.dp.pause();
+          } else {
+            this.ignoreEvents.play++;
+            this.dp.play();
+          }
+          this.ignoreEvents.seek++;
+          this.dp.seek(event.time);
+          break;
+      }
+    },
+    sendControl(action) {
+      this.socket.emit(
+        "video-control",
+        JSON.stringify({
+          user: this.userID,
+          action: action,
+          time: this.dp.video.currentTime,
+          src: this.dp.video.currentSrc,
+          paused: this.dp.video.paused,
+        })
+      );
+    },
+  },
+
+  mounted() {
+    this.userID = this.randomString(10);
+    this.socket = io();
+    this.dp = new DPlayer({
+      container: document.getElementById("dplayer"),
+      screenshot: true,
+      volume: 0,
+      video: {
+        type: "auto",
+      },
+      contextmenu: [
+        {
+          text: "Sync",
+          click: (player) => {
+            this.sendControl("sync");
+            player.notice("synced", 2000, 0.8);
+          },
+        },
+      ],
+    });
+
+    this.socket.on("video-control", (res) => {
+      const result = JSON.parse(res);
+      if (result.user !== this.userID) {
+        this.resultHandler(this.dp, result);
+      }
+    });
+
+    this.dp.on("play", () => {
+      if (this.ignoreEvents.play > 0) {
+        this.ignoreEvents.play--;
+        return;
+      }
+      this.sendControl("play");
+    });
+    this.dp.on("pause", () => {
+      if (this.ignoreEvents.pause > 0) {
+        this.ignoreEvents.pause--;
+        return;
+      }
+      this.sendControl("pause");
+    });
+    // dp.on("progress", (event) => {
+    //   console.log("progress", event);
+    // });
+    this.dp.on("seeked", () => {
+      if (this.ignoreEvents.seek > 0) {
+        this.ignoreEvents.seek--;
+        return;
+      }
+      this.sendControl("seek");
+    });
+    this.dp.on("ratechange", () => {
+      if (this.ignoreEvents.ratechange > 0) {
+        this.ignoreEvents.ratechange--;
+        return;
+      }
+      console.log("ratechange");
+    });
+  },
+};
 </script>
