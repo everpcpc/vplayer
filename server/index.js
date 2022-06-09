0d42edb7e980f6ea0d4e10ef13f1cccf55ea767c
everpcpc <git@everpcpc.com>
2022-06-09 20:27:35 +0800

    feat: add yargs for server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 const server = require('http').Server();
 const fs = require("fs")
 const path = require("path")
+
 const io = require('socket.io')(server);
+const yargs = require('yargs');
 
-const getAllFiles = function (dirPath, arrayOfFiles) {
+const argv = yargs
+    .option('port', {
+        alias: 'p',
+        type: 'number',
+        default: 2233,
+        description: 'the port listening on',
+    })
+    .option('data', {
+        alias: 'd',
+        type: 'string',
+        default: '/data/movie',
+        description: 'data dir for scanning videos',
+    })
+    .help().alias('help', 'h')
+    .argv;
+
+function getAllFiles(dirPath, arrayOfFiles) {
     files = fs.readdirSync(dirPath)
-
     arrayOfFiles = arrayOfFiles || []
-
     files.forEach(function (file) {
         if (fs.statSync(dirPath + "/" + file).isDirectory()) {
             arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
@@ -15,12 +31,8 @@ const getAllFiles = function (dirPath, arrayOfFiles) {
             arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
         }
     })
-
     return arrayOfFiles
 }
-
-const port = 2233;
-
 
 io.on('connection', (socket) => {
     console.log('new connection');
@@ -36,12 +48,12 @@ io.on('connection', (socket) => {
 
     socket.on('browse', (callback) => {
         console.log('received: browse');
-        const files = getAllFiles("./movie/")
+        const files = getAllFiles(argv.data)
         callback(files);
     });
 
 });
 
-server.listen(port, () => {
-    console.log(`listen on :${port}`);
+server.listen(argv.port, () => {
+    console.log(`listen on :${argv.port}, with data dir: ${argv.data}`);
 });
